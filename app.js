e5fb56175e7299ed260f9372bf8f0c7ea490de98
Ahmad Pouramini <pouramini@gmail.com>
2024-11-06 16:37:58 +0330

    commit

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+// server.js
+const strapi = require('strapi');
+strapi().start();
+
